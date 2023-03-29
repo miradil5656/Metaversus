aa43261b5d9df1ede1e219d4ab2f4e4b114fd907
miradil5656 <miradild404@gmail.com>
2023-03-29 23:59:07 +0900

    add hero section

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,9 +1,24 @@
 'use client';
 
+import { motion } from 'framer-motion';
+import styles from '../styles';
+import { navVariants } from '../utils/motion';
+
 const Navbar = () => (
-  <nav className="text-green-500">
-    navbar
-  </nav>
+  <motion.nav
+    variants={navVariants}
+    initial="hidden"
+    whileInView="show"
+    className={`${styles.xPaddings} py-8 relative`}
+  >
+    <div className=" absolute w-[50%] inset-0 gradient-01" />
+    <div className={`${styles.innerWidth} mx-auto flex justify-between items-center gap-8`}>
+      <img src="/search.svg " alt="search" className="w-[24px] h-[24px] object-contain" />
+      <h2 className="text-[24px] font-extrabold leading-[30px] text-white">METAVERSUS</h2>
+      <img src="/menu.svg" alt="menu" className="w-[24px] h-[24px] object-contain" />
+    </div>
+  </motion.nav>
+
 );
 
 export default Navbar;

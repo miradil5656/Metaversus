89ec25733ff95698e5c9500792a8878dc6b7232a
miradil5656 <miradild404@gmail.com>
2023-04-01 10:40:09 +0900

    remove h1

--- sections/Hero.jsx
+++ sections/Hero.jsx
@@ -14,12 +14,6 @@ const Hero = () => (
       className={`${styles.innerWidth} mx-auto flex flex-col`}
     >
       <div className="flex justify-center items-center flex-col relative z-10">
-        <motion.h1
-          variants={textVariant(1.1)}
-          className={styles.heroHeading}
-        >
-          METAVERSUS
-        </motion.h1>
         <motion.div
           variants={textVariant(1.2)}
           className="flex flex-row justify-center items-center"
